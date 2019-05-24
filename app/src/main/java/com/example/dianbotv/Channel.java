b41864d357823c33d0f35eac6004d05fc76d48ec
yuchangbin <2449393676@qq.com>
2019-05-24 14:53:52 +0800

    Initial commit 2019/5/24 updata

---
+++ app/src/main/java/com/example/dianbotv/Channel.java
@@ -0,0 +1,33 @@
+package com.example.dianbotv;
+
+import java.io.Serializable;
+
+public class Channel implements Serializable { //设置接口
+    private String title;
+    private String quality;
+    private String url;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ app/src/main/java/com/example/dianbotv/bofangActivity.java
@@ -0,0 +1,31 @@
+package com.example.dianbotv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+
+public class bofangActivity extends AppCompatActivity {
+    PlayerView bofang;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bofang);
+        bofang = findViewById(R.id.bofang);
+
+        //1. 创建播放器
+        ExoPlayer player = ExoPlayerFactory.newSimpleInstance(this);
+        player.setPlayWhenReady(true);
+        bofang.setPlayer(player);
+        //2.指定播放源
+        DataSource.Factory factory = new DefaultDataSourceFactory(this, "DDPlayer");
+        Uri uri = Uri.parse("http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8");
+        HlsMediaSource source = new HlsMediaSource.Factory(factory).createMediaSource(uri);
+        player.prepare(source);
+    }
+}
